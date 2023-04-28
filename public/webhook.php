88f79d54d41b810feeea86f5af30d4a178f69b66
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-04-28 10:28:40 -0600

    remove webhook, why is this so hard

--- public/webhook.php
+++
@@ -1,7 +0,0 @@
-<?php
-$command = 'cd /var/www/gsjohnson/ && git reset --hard HEAD && git clean -d -f && git pull origin master && php artisan migrate';
-$output = [];
-exec($command, $output);
-foreach ($output as $command_result) {
-    echo $command_result . "<br>";
-}
