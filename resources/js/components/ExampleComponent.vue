44e017186685950c823a80366224d4ad294d5fee
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-04-28 09:52:27 -0600

    Does webhook work properly?

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,7 +3,7 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header">Example Component</div>
+          <div class="card-header">Test change</div>
 
           <div class="card-body">Hi Sugar!</div>
         </div>
