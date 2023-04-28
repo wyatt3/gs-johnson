c6fbb2f7b74e77503829a790a9e020204d133b51
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-04-28 10:04:52 -0600

    remove npm stuff from webhook because it's too hard to get working

--- public/webhook.php
+++ public/webhook.php
@@ -1,5 +1,5 @@
 <?php
-$command = 'cd /var/www/gsjohnson/ && git reset --hard HEAD && git clean -d -f && git pull origin master && php artisan migrate && npm install && npm run prod';
+$command = 'cd /var/www/gsjohnson/ && git reset --hard HEAD && git clean -d -f && git pull origin master && php artisan migrate';
 $output = [];
 exec($command, $output);
 foreach ($output as $command_result) {
