0d8985eba08e417b497d0fa505b3f34c48988ea2
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-05-02 13:41:34 -0600

    Fix reordering projects

--- app/Models/ProjectCategory.php
+++ app/Models/ProjectCategory.php
@@ -16,6 +16,6 @@ class ProjectCategory extends Model
 
     public function projects()
     {
-        return $this->hasMany(Project::class, 'category_id', 'id');
+        return $this->hasMany(Project::class, 'category_id', 'id')->orderBy('order');
     }
 }
