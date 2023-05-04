8ea498666089aecb59c287c19a0d1bac5064d788
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-05-04 09:36:13 -0600

    update resume check

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <h1 class="text-gold-main">Admin</h1>
-@if(file_exists(asset('storage/GracieJohnson.pdf')))
+@if(file_exists(public_path('/storage/GracieJohnson.pdf')))
 <h2 class="text-gold-main">Resume</h2>
-<iframe class="w-100 mb-3" src="{{ asset('storage/GracieJohnson.pdf') }}" frameborder="0" style="height: 70vw"></iframe>
+<iframe class="w-100 mb-3" src="{{ asset('/storage/GracieJohnson.pdf') }}" frameborder="0" style="height: 70vw"></iframe>
 @endif
 <h3>Upload New Resume</h3>
 <form class="form-group" action="{{ route('admin.resume.update') }}" method="POST" enctype="multipart/form-data">
