a176bbfcbd448671257b6f8ab91c84fa1c43beaa
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-05-04 10:07:30 -0600

    Start on social link update functionality

--- app/Services/AdminService.php
+++ app/Services/AdminService.php
@@ -17,4 +17,22 @@ class AdminService
         $resume->storeAs('public/', 'GracieJohnson.pdf');
         return true;
     }
+
+    /**
+     * update social links
+     *
+     * @param string $filepath
+     * @param string $displayName
+     * @param string $url
+     * @return bool
+     */
+    public function updateSocialLinks(string $filename, string $displayName, string $url): bool
+    {
+        $json = json_encode([
+            'displayName' => $displayName,
+            'url' => $url
+        ]);
+        file_put_contents(__DIR__ . "/../../social/$filename.json", $json);
+        return true;
+    }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,20 +2,43 @@
 
 @section('content')
 <h1>Admin</h1>
-@if(file_exists(public_path('/storage/GracieJohnson.pdf')))
-<h2>Resume</h2>
-<iframe class="w-100 mb-3" src="{{ asset('/storage/GracieJohnson.pdf') }}" frameborder="0" style="height: 70vw"></iframe>
-@endif
-<h3>Upload New Resume</h3>
-<form class="form-group" action="{{ route('admin.resume.update') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input class="mt-3 form-control" name="resume" type="file">
-    <input class="btn btn-primary mt-3" type="submit" value="Upload">
-</form>
+<div class="resume mb-4">
+    @if(file_exists(public_path('/storage/GracieJohnson.pdf')))
+    <h2>Resume</h2>
+    <iframe class="w-100 mb-3" src="{{ asset('/storage/GracieJohnson.pdf') }}" frameborder="0" style="height: 70vw"></iframe>
+    @endif
+    <h3>Upload New Resume</h3>
+    <form class="form-group" action="{{ route('admin.resume.update') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input class="mt-3 form-control" name="resume" type="file">
+        <input class="btn btn-primary mt-3" type="submit" value="Upload">
+    </form>
+</div>
+<div class="social-links">
+    <h2 class="mb-3">Social Links</h2>
 
-<h2>Social Links</h2>
+    <div class="d-flex justify-content-evenly">
+        <div class="left-side">
+            <h3 class="text-center">Left Side</h3>
+            <form>
+                <label>Displayed Name</label>
+                <input class="form-control mb-2" type="text" name="name" placeholder="Displayed Name">
+                <label>URL</label>
+                <input class="form-control mb-2" type="text" name="url" placeholder="URL">
+                <input class="btn btn-primary" type="submit" value="Update">
+            </form>
+        </div>
+        <div class="right-side">
+            <h3 class="text-center">Right Side</h3>
+            <form>
+                <label>Displayed Name</label>
+                <input class="form-control mb-2" type="text" name="name" placeholder="Displayed Name">
+                <label>URL</label>
+                <input class="form-control mb-2" type="text" name="url" placeholder="URL">
+                <input class="btn btn-primary" type="submit" value="Update">
+            </form>
+        </div>
+    </div>
 
-<h3>Left Side</h3>
-
-<h3>Right Side</h3>
+</div>
 @endsection
