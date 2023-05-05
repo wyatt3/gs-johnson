9674662301f31b2769d00f672719fe3ca564527c
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-05-05 10:36:25 -0600

    more mobile styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<header class="bg-secondary text-gold-main text-center pt-4 pb-3">
+<header class="bg-secondary text-gold-main text-center pb-3">
     <img width="100%" src="{{ asset('img/ornament.png') }}" aria-hidden>
     <h1 class="title">Gracie Johnson</h1>
     <span class="subtitle">Design Portfolio</span>
