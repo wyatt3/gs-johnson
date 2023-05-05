f6a06fac3ee25ccc490fe25dc7c9ae322c375fab
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-05-05 17:27:14 -0600

    started styling nav

--- resources/js/components/public/CategorySection.vue
+++ resources/js/components/public/CategorySection.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="tabs">
-      <ul>
+    <div class="tabs bg-primary py-2">
+      <ul class="d-flex justify-content-evenly mb-0 p-0">
         <li v-for="tab in tabs" :key="tab.id">
           <a
             :class="{ active: activeTab === tab.id }"
