63f7da6f7ab637ae91364646e2337bb1d220007a
Wyatt Johnson <wyatt.j1834@gmail.com>
2026-01-17 16:59:20 -0700

    modernize js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,21 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import "./bootstrap";
+import 'bootstrap';
+import { createApp } from 'vue';
 
-require('./bootstrap');
+import CategoryTable from "./components/categories/CategoryTable.vue";
+import Projects from "./components/projects/Projects.vue";
+import FileUpload from "./components/FileUpload.vue";
+import ProjectMedia from "./components/projects/ProjectMedia.vue";
 
-window.Vue = require('vue').default;
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import CategorySection from "./components/public/CategorySection.vue";
 
 
-// Admin Components
-Vue.component('CategoryTable', require('./components/categories/CategoryTable.vue').default);
-Vue.component('Projects', require('./components/projects/Projects.vue').default);
-Vue.component('FileUpload', require('./components/FileUpload.vue').default);
-Vue.component('projectMedia', require('./components/projects/ProjectMedia.vue').default);
+const app = createApp();
 
-
-// Public Components
-Vue.component('CategorySection', require('./components/public/CategorySection.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+app
+    .component('category-table', CategoryTable)
+    .component('category-section', CategorySection)
+    .component('projects', Projects)
+    .component('file-upload', FileUpload)
+    .component('project-media', ProjectMedia)
+    .mount("#app");
