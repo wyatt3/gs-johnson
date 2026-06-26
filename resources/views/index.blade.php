da3581061628eb94b3a62a843b5d4d4ff10f1f4a
Wyatt Johnson <wyatt.j1834@gmail.com>
2026-06-26 14:18:08 -0600

    some footer work

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 @include('partials.header')
 <div class="homepage-divider bg-primary"></div>
 <carousel :categories="{{ $categories }}"></carousel>
-<footer class="bg-secondary d-flex justify-content-center py-2">
+<footer class="bg-secondary d-flex justify-content-center align-items-center py-2">
     <div class="footer-divider"></div>
     @foreach($footerLinks as $link)
     <a href="{{ $link->url }}" download class="footer-link footer-link-center">{{ $link->name }}</a>
