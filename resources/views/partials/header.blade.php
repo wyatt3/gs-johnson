933e9c15a8f02a39cc48e49fc222bc0a1d0106e4
Wyatt Johnson <wyatt.j1834@gmail.com>
2026-08-18 19:52:07 -0600

    fix and extract header css

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,15 @@
 <header class="header bg-secondary">
-    <div class="rhombus white-rhombus rhombus-1"></div>
-    <div class="rhombus white-rhombus rhombus-2"></div>
-    <div class="rhombus white-rhombus rhombus-3"></div>
-    <div class="rhombus rhombus-4 bg-secondary"></div>
+    <div class="left-rhombuses">
+        <div class="rhombus bg-transparent-white rhombus-1"></div>
+        <div class="rhombus bg-transparent-white rhombus-2"></div>
+        <div class="rhombus bg-transparent-white rhombus-3"></div>
+        <div class="rhombus rhombus-4 bg-secondary"></div>
+    </div>
     <img height="190px" class="header-img" src="img/flowers.jpg" alt="logo">
-    <div class="rhombus rhombus-5"></div>
-    <div class="rhombus rhombus-6 bg-secondary"></div>
+    <div class="right-rhombuses">
+        <div class="rhombus rhombus-5"></div>
+        <div class="rhombus rhombus-6 bg-secondary"></div>
+    </div>
     <div class="header-title">
         <h1>
             <span class="art-and">Art &amp;</span>
@@ -14,3 +18,8 @@
         <div class="from-gracie">from gracie johnson</div>
     </div>
 </header>
+
+<?php
+
+
+?>
